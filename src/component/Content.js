98fd4dc9746b9031709a75dc65c17298504cb635
SA K <sakang07@gmail.com>
2022-05-20 13:29:28 +0900

    [style] Resolve the twisted box #1

--- src/component/Content.js
+++ src/component/Content.js
@@ -2,18 +2,18 @@
 // Content.js
 // 220119
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import '../style/Content.scss';
-import defaultCat from '../img/cat_default.png';
-import cryCat from '../img/cat_cry.png';
-import sleepCat from '../img/cat_sleep.png';
+import "../style/Content.scss";
+import defaultCat from "../img/cat_default.png";
+import cryCat from "../img/cat_cry.png";
+import sleepCat from "../img/cat_sleep.png";
 
 function Content() {
   const catArr = [defaultCat, cryCat, sleepCat];
 
   const [cat, setCat] = useState(catArr[0]);
-  const [act, setAct] = useState('stay');
+  const [act, setAct] = useState("stay");
 
   const [sec, setSec] = useState(0);
   const [min, setMin] = useState(0);
@@ -36,25 +36,23 @@ function Content() {
       console.log(sec);
     }, 1000);
     return () => clearInterval(countDown);
-  }, [sec, min])
-
+  }, [sec, min]);
 
   const fnStart = () => {
-    if(act === 'stay') {
+    if (act === "stay") {
       // 고양이 클릭하면 한번 울고 타이머 시작
-      setAct('run');
+      setAct("run");
 
       setCat(catArr[1]);
       setTimeout(() => {
         setCat(catArr[0]);
       }, 500);
-      
+
       // 클릭시 기능 추가
       setMin(30);
-
-    } else if (act === 'pause') {
+    } else if (act === "pause") {
       // 고양이가 자고 있을때 클릭하면 한번 울고 타이머 시작
-      setAct('run');
+      setAct("run");
 
       setCat(catArr[1]);
       setTimeout(() => {
@@ -65,34 +63,43 @@ function Content() {
 
   const fnDrag = () => {
     // 타이머 작동중일 때 고양이 쓰다듬으면 타이머 일시정지
-    if(act === 'run') {
-      setAct('pause');
+    if (act === "run") {
+      setAct("pause");
       setCat(catArr[2]);
       // clearInterval(countDown);
     }
-  }
-
+  };
 
   return (
-    <section id="contentBox">
+    <section id='contentBox'>
       <div className='cat_area'>
         <h2 className='blind'>시간 알려주는 고양이</h2>
-        <button type='button' className='default' onClick={fnStart} onDrag={fnDrag}><img src={cat} className={act} alt='default cat' /></button>
+        <button type='button' className='default' onClick={fnStart} onDrag={fnDrag}>
+          <img src={cat} className={act} alt='default cat' />
+        </button>
       </div>
 
       <div className='timer_area'>
         <h2 className='blind'>타이머</h2>
-        <p>{min < 10 ? `0${min}` : min} : {sec < 10 ? `0${sec}` : sec}</p>
+        <p>
+          {min < 10 ? `0${min}` : min} : {sec < 10 ? `0${sec}` : sec}
+        </p>
       </div>
 
       <div className='collect_area'>
-        <h2>고양이가 <span className='num'>{num}</span>개 주워왔어요!</h2>
+        <h2>
+          고양이가 <span className='num'>{num}</span>개 주워왔어요!
+        </h2>
         <ul className='collect_inner'>
-          {collectArr.map((el, idx) => <li key={idx}><span>{`물건 ${idx}`}</span></li>)}
+          {collectArr.map((el, idx) => (
+            <li key={idx}>
+              <span>{`물건 ${idx}`}</span>
+            </li>
+          ))}
         </ul>
       </div>
     </section>
-  )
+  );
 }
 
-export default Content
+export default Content;
