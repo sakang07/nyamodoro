78aeb1472eb163bc7a6dd56f2cf07501c2842a4f
SA K <sakang07@gmail.com>
2022-10-07 22:49:07 +0900

    [feat] Set the development environment - Add typescript, styled-components, eslint, prettier, etc

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Header from "./components/Header";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,36 @@
-import React, { useState } from "react";
-import "../style/Header.scss";
+import React, { useState } from 'react';
+import '../style/Header.scss';
 
-import onImg from "../assets/on.png";
-import offImg from "../assets/off.png";
+import onImg from '../assets/on.png';
+import offImg from '../assets/off.png';
 
 function Header() {
   const offBtn = { backgroundImage: `url('${offImg}')` };
   const onBtn = { backgroundImage: `url('${onImg}')` };
 
-  const [isOn, setIsOn] = useState(false);
+  const [isAutoOn, setIsAutoOn] = useState(false);
+  const [isHelpOn, setIsHelpOn] = useState(false);
 
   const handleAuto = () => {
-    if (!isOn) {
-      setIsOn(true);
-    } else {
-      setIsOn(false);
-    }
+    isAutoOn ? setIsAutoOn(false) : setIsAutoOn(true);
+  };
+
+  const handleClick = () => {
+    isHelpOn ? setIsHelpOn(false) : setIsHelpOn(true);
   };
 
   return (
-    <header id='headBox'>
-      <h1 className='blind'>nyamodoro timer</h1>
+    <header id="headBox">
+      <h1 className="blind">nyamodoro timer</h1>
 
-      <div className='btn_area'>
-        <button type='button' style={isOn ? onBtn : offBtn} onClick={handleAuto}>
+      <div className="btn_area">
+        <button type="button" style={isAutoOn ? onBtn : offBtn} onClick={handleAuto}>
           <span>자동 급식기</span>
         </button>
       </div>
 
-      <div className='help_area'>
-        <button type='button'>
+      <div className="help_area">
+        <button type="button" onClick={handleClick}>
           <span>도움말</span>
         </button>
       </div>
